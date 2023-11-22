b78c6fe70f93fba1a9cc2682e4ef41f9fde4b800
olaxkid 911 <lukmanuthmanabubakr>
2023-11-23 00:57:10 +0100

    third commit

---
+++ Backend/Controllers/UserController.js
@@ -0,0 +1,7 @@
+const registerUser = async (req, res) => {
+    res.send("Resgister User")
+}
+
+module.exports = {
+    registerUser
+}
--- Backend/Server.js
+++ Backend/Server.js
@@ -2,14 +2,35 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const userRoutes = require("./Routes/UserRoutes")
 
 const app = express();
+
+
+//Middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(
+    cors({
+        origin:["http://localhost:3000", "https://authz-app.versel.app"],
+        credentials: true
+    })
+)
+
+//Routes 
+app.use("/api/users", userRoutes)
+
 
 app.get("/", (req, res) => {
     res.send("Home Page");
 });
 
-const PORT = process.env.PORT || 5000; // Corrected the default port
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.MONGO_URI)
